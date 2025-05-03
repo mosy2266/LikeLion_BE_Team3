06558185ef231ade56900177e157a0a2fed8aa89
Yun <mosy2266@naver.com>
2025-05-03 19:25:11 +0900

    브랜치 생성

---
+++ be_study/src/main/java/yun/likelion/be_study/BeStudyApplication.java
@@ -0,0 +1,13 @@
+package yun.likelion.be_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeStudyApplication.class, args);
+    }
+
+}
---
+++ be_study/src/test/java/yun/likelion/be_study/BeStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package yun.likelion.be_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
