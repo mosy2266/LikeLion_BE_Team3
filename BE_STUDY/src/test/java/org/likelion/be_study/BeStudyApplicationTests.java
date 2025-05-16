c1dcd0485458219292a046650a8f5c5239772887
jbh010204 <j@2180130>
2025-05-16 23:47:26 +0900

    chore: 프로젝트 초기 설정

---
+++ BE_STUDY/src/test/java/org/likelion/be_study/BeStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.likelion.be_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
